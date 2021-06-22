f1b831b00a1513ffacedd42b8d77deb85a40fcdf
Stanislas Lejay <p1kachu@lse.epita.fr>
2021-06-22 14:45:07 +0900

    Add helper for P30 bitvectors
    Signed-off-by: Stanislas Lejay <p1kachu@lse.epita.fr>

--- ida/ida-helper.py
+++ ida/ida-helper.py
@@ -6,10 +6,12 @@
 from idaapi import *
 from idautils import *
 
+MIN_RANGE=0
+MAX_RANGE=0x6000
 
 def define_p30_ecu_locations():
     '''
-    Mark known addresses and locations in the code
+    Mark known addresses and locations in the code (P30 ECU only)
     (Completion of the generic _init_memory() function from the loader, but
     with P30 ECU addresses from www.pgmfi.org/twiki/bin/view/Library/P30.html)
     '''
@@ -129,6 +131,118 @@ def make_loc_out_of_jumps(range_low, range_high):
         i += insn.size
         continue
 
+def helper_find_code_or_data_that_match(all_of_this, min_range, max_range):
+
+    '''
+    Find code/data in disassembly based on text representation only
+    example:
+
+        helper_find_code_or_data_that_match(["mb", "off("], 0, 6000)
+            Found 78 values matching ['mb', 'off(']
+            0x500: mb      off(22h).1, c ()
+            0x5a2: mbr     c, off(0cbh) ()
+            0x6d7: mb      off(1eh).0, c ()
+            0x724: mb      off(2bh).5, c ()
+            0x78b: mb      off(1bh).7, c ()
+            0x795: mb      off(1bh).6, c ()
+            0x1580: mb      off(27h).0, c# injector test bypass #2 (change to j label_159a(03 9a 15)) ()
+            0x158c: mb      off(27h).5, c ()
+            ...
+
+    '''
+
+    assert(min_range < max_range)
+    assert(type(all_of_this) == list)
+
+    all_of_this = [elt.lower() for elt in all_of_this]
+    values = []
+    i = min_range
+    while i < max_range:
+        disasm = GetDisasmEx(i, 0).lower()
+        match = True
+        for elt in all_of_this:
+            if elt not in disasm:
+                match = False
+                break
+        if match:
+            values.append((i, disasm))
+
+        add = ItemSize(i)
+        i += add if add else 1
+
+    if len(values):
+        print("Found {0} values matching {1}".format(len(values), all_of_this))
+    for elt in values:
+        print("{0}: {1} ({2})".format(hex(elt[0])[:-1], elt[1], GetFunctionName(elt[0])))
+
+    return values
+
+def comment_known_p30_bitvectors(min_range=MIN_RANGE, max_range=MAX_RANGE):
+    '''
+    Add comment to known bitvector values to help the disassembly (P30 ECU only)
+    '''
+
+    for x in helper_find_code_or_data_that_match(["off(16h).4"], min_range, max_range):
+        MakeRptCmt(x[0], "VTEC enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(27h).6"], min_range, max_range):
+        MakeRptCmt(x[0], "Knock sensor enable enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).6"], min_range, max_range):
+        MakeRptCmt(x[0], "O2 heater sensor enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(27h).4"], min_range, max_range):
+        MakeRptCmt(x[0], "Barometric sensor enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(19h).1"], min_range, max_range):
+        MakeRptCmt(x[0], "O2 sensor enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(17h).3"], min_range, max_range):
+        MakeRptCmt(x[0], "Injector test (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(27h).7"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6007 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).1"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6008 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).7"], min_range, max_range):
+        MakeRptCmt(x[0], "EGR system (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(27h).1"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_600A (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).3"], min_range, max_range):
+        MakeRptCmt(x[0], "Auto/Manual enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).2"], min_range, max_range):
+        MakeRptCmt(x[0], "Speed limiter enable (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).0"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6014 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(17h).6"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6015 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(16h).5"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6016 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["PSWL.4"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6017 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(27h).3"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6018 (maybe byte_600C too) (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(27h).2"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_6019 (bitvector)")
+
+    for x in helper_find_code_or_data_that_match(["off(19h).1"], min_range, max_range):
+        MakeRptCmt(x[0], "byte_601A (bitvector)")
+
+
+
+
+
 
 ################ Main
 # Mark all the bytes in DATA as data
@@ -136,3 +250,6 @@ def make_loc_out_of_jumps(range_low, range_high):
 #        create_data(i, FF_BYTE, 1, 0)
 #make_loc_out_of_jumps(0, 0x6000)
 #define_p30_ecu_locations()
+#comment_known_p30_bitvectors()
+
+find_code_or_data_that_match(["27h).7"])
